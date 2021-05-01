46694f18766fe187d8223856f8ff6a6eb84bbaab
chrisdcao <khanhcao96@gmail.com>
2021-05-01 22:21:55 +0700

    update the theme to change the whole profile based on system theme

--- home/auto_darkmode_theme.sh
+++ home/auto_darkmode_theme.sh
@@ -2,11 +2,19 @@
 # set the cursor color to be changable
 # gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-colors-set true
 # calling variable for later usage
-echo -ne '\e[5 q'
 defaultprofile=$(gsettings get org.gnome.Terminal.ProfilesList default)
 profileID=$(echo "$defaultprofile" | cut -d "'" -f 2)
 theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 # random=$((1 + $RANDOM % 10))
+# I-Beam cursorshape on each new session. Other escape sequences include:
+# \e[0 q or \e[ q: reset to whatever's defined in the profile settings
+# \e[1 q: blinking block
+# \e[2 q: steady block
+# \e[3 q: blinking underline
+# \e[4 q: steady underline
+# \e[5 q: blinking I-beam
+# \e[6 q: steady I-beam
+echo -ne '\e[1 q'
 
 # if [[ $(pgrep terminal) != '' ]]
 # then
@@ -15,25 +23,35 @@ theme=$(gsettings get org.gnome.desktop.interface gtk-theme)
 
 if [[ $theme == "'WhiteSur-dark-alt'" ]]
 then
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ background-color 'rgb(40,40,40)'
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ foreground-color 'rgb(251,241,199)'
+    # shades_of_purple
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-colors-set true 
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-background-color '#7d44b2'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-foreground-color '#E1EFFF'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ background-color 'rgb(45,43,85)'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ foreground-color 'rgb(225,239,255)'
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-colors-set true
-    # gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-background-color 'rgb(251,241,199)' 
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-background-color 'rgb(255,238,238)' 
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-foreground-color 'rgb(0,0,0)' 
-    # gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-foreground-color 'rgb(40,40,40)'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-background-color 'rgb(255,208,0)' 
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ bold-is-bright true
+    dconf write /org/gnome/terminal/legacy/profiles:/:0b0410a3-164b-4b3c-8d11-b104a8bec905/palette "['rgb(0,0,0)', 
+                                                                                                     'rgb(236,58,55)',  
+                                                                                                     'rgb(58,217,0)', 
+                                                                                                     'rgb(250,208,0)', 
+                                                                                                     'rgb(105,67,255)', 
+                                                                                                     'rgb(255,44,112)', 
+                                                                                                     'rgb(128,252,255)', 
+                                                                                                     'rgb(255,255,255)', 
 
-    # gsettings set org.gnome.desktop.interface monospace-font-name 'Cartograph CF Semi-Bold 10'
-    # if [[ $random -lt 4 ]]
-    # then
-        # cat ~/starwars6.txt
-    # elif [[ $random -ge 4 && $random -lt 7 ]]
-    # then
-        # cat ~/ship.txt
-    # else
-        # cat ~/ship2.txt
-    # fi
+                                                                                                     'rgb(92,92,97)', 
+                                                                                                     'rgb(236,58,55)', 
+                                                                                                     'rgb(117,228,76)', 
+                                                                                                     'rgb(250,208,0)', 
+                                                                                                     'rgb(105,67,255)', 
+                                                                                                     'rgb(251,148,255)', 
+                                                                                                     'rgb(128,252,255)', 
+                                                                                                     'rgb(45,43,85)']"
+    gsettings set org.gnome.desktop.interface monospace-font-name 'Fixedsys Excelsior 12'
+
     echo $theme > ~/vim_mode.txt
     if [[ -n "$PS1" ]] && [[ -z "$TMUX" ]]
     then
@@ -51,13 +69,36 @@ then
     # trap change_theme 0
 
 else
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ foreground-color 'rgb(40,40,40)'
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ background-color 'rgb(251,241,199)'
+    # xcode ( old version )
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-colors-set true 
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-background-color '#2d9eff'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ highlight-foreground-color '#ffffff'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ foreground-color 'rgb(0,0,0)'
+    # gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ background-color 'rgb(251,241,199)'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ background-color 'rgb(255,255,255)'
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-colors-set true
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-foreground-color 'rgb(251,241,199)' 
-    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-background-color 'rgb(40,40,40)'
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-foreground-color 'rgb(255,255,255)' 
+    gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ cursor-background-color 'rgb(0,0,0)'
     gsettings set org.gnome.Terminal.Legacy.Profile:/org/gnome/terminal/legacy/profiles:/:$profileID/ bold-is-bright false
     # gsettings set org.gnome.desktop.interface monospace-font-name 'Cartograph CF Regular 10'
+    dconf write /org/gnome/terminal/legacy/profiles:/:0b0410a3-164b-4b3c-8d11-b104a8bec905/palette "['rgb(38,38,38)', 
+                                                                                                     'rgb(209,47,27)', 
+                                                                                                     'rgb(45,133,4)', 
+                                                                                                     'rgb(120,73,42)', 
+                                                                                                     'rgb(15,104,160)', 
+                                                                                                     'rgb(128,79,184)', 
+                                                                                                     'rgb(62,128,135)', 
+                                                                                                     'rgb(180,216,253)', 
+
+                                                                                                     'rgb(38,38,38)', 
+                                                                                                     'rgb(209,47,27)', 
+                                                                                                     'rgb(45,133,4)', 
+                                                                                                     'rgb(120,73,42)', 
+                                                                                                     'rgb(15,104,160)', 
+                                                                                                     'rgb(128,79,184)', 
+                                                                                                     'rgb(62,128,135)', 
+                                                                                                     'rgb(180,216,253)']"
+    gsettings set org.gnome.desktop.interface monospace-font-name 'Fixedsys Excelsior 12'
     echo $theme > ~/vim_mode.txt
 
     if [[ -n "$PS1" ]] && [[ -z "$TMUX" ]]
